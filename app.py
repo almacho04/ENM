bfe5f6cdf652ab979e19bf99d9a11bed1d855e1b
Makhmud Assentay <200107067@stu.sdu.edu.kz>
2023-08-03 17:15:34 +0600

    1) numpy array dist_mat was decleared 2) written implementaation for dist_mat (def _calculate_dist_mat) 3) written implementaation for calculate dis between two 3D points. (def distance_function) 4) written implementaation for Kirhoff matrix exponetial distance dependence (def _get_K_exp_dist)

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from enm import *
 a = ENM('1znw.pdb')
 a = a.filter('CA')
+b = ENM(a.getCoords())
+H = b.getHessian(adj = 'delaunay')
+
 
 print(a.atoms)
--- enm/enm.py
+++ enm/enm.py
@@ -1,3 +1,5 @@
+from turtle import distance
+
 import numpy as np
 
 from Bio.PDB import PDBIO, parse_pdb_header, Structure, Model, Chain, Residue, Atom 
@@ -14,6 +16,7 @@ import re
 
 class ENM:
 	def __init__(self, pdb_obj = None, **kwargs):
+		self.dist_mat = None
 		self.atoms = []
 		self.info = ' -> ENM Object\n'
 
@@ -34,6 +37,17 @@ class ENM:
 						self.atoms.append(Node(atom))
 		except:
 			raise TypeError(f'Invalid type --> {type(pdb_obj)}')
+
+		self.dist_mat = self._calculate_dist_mat()
+
+	def _calculate_dist_mat(self):
+		coordinates = self.getCoords()
+		num_atoms = len(coordinates)
+		dist_mat = np.zeros((num_atoms, num_atoms))
+		for i in range(num_atoms):
+			for j in range(num_atoms):
+				dist_mat[i, j] = self.distance_function(coordinates[i], coordinates[j])
+		return dist_mat
 
 	def __repr__(self):
 		return self.info
@@ -116,7 +130,12 @@ class ENM:
 		elif adj_type == 'cutOff':
 			adj = self._adjacancyMatrix(coordinateArray, kwargs.get('cutOff', 11))
 		self.edges = adj
+
 		K = self._get_K_homo(adj, 1)
+
+		#Example to create Kirhoff matrix with exp dis dependence
+		K_exp_deis = self._get_K_exp_dist(adj,1)
+
 		nij = self._get_nij(coordinateArray)
 		grad = self._get_grad(adj)
 		D = self._get_D(grad, nij)
@@ -149,6 +168,16 @@ class ENM:
 	def _get_K_homo(self, adj, k):
 		bonds = np.array([(i, j) for i, j in zip(*np.where(adj)) if i > j])
 		return np.eye(len(bonds))
+
+	def distance_function(self,point1, point2):
+		# Calculate Euclidean distance between two points
+		return np.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2 + (point1[2] - point2[2]) ** 2)
+
+	def _get_K_exp_dist(self, adj, k0):
+		bonds = np.array([k0 * np.exp(-self.dist_mat[i][j]) for i, j in zip(*np.where(adj)) if i > j])
+		K_distance = np.zeros((len(bonds), len(bonds)))
+		np.fill_diagonal(K_distance, bonds)
+		return K_distance
 
 	def _get_nij(self, coord, d = 3):
 		N = len(coord)
